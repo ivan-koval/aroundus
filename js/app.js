84f61287e009b4c2e7abebdafe80119accb2af3f
Ivanna Kravchuk <hurtpity@gmail.com>
2014-11-25 22:56:04 +0200

    Some changes

--- js/app.js
+++ js/app.js
@@ -31,28 +31,67 @@ angular.module('myApp', [
                 templateUrl: 'views/give_gift.html'
             })
             .when('/1996_97', {
-                templateUrl: 'views/1996_97.html'
+                templateUrl: 'views/Leters/1996_97.html'
     })
             .when('/1998', {
-                templateUrl: 'views/1998.html'
+                templateUrl: 'views/Leters/1998.html'
             })
             .when('/1999', {
-                templateUrl: 'views/1999.html'
+                templateUrl: 'views/Leters/1999.html'
             })
             .when('/2000_01', {
-                templateUrl: 'views/2000_01.html'
+                templateUrl: 'views/Leters/2000_01.html'
             })
             .when('/2002', {
-                templateUrl: 'views/2002.html'
+                templateUrl: 'views/Leters/2002.html'
             })
             .when('/2003', {
-                templateUrl: 'views/2003.html'
+                templateUrl: 'views/Leters/2003.html'
             })
             .when('/2004', {
-                templateUrl: 'views/2004.html'
+                templateUrl: 'views/Leters/2004.html'
             })
             .when('/2005', {
-                templateUrl: 'views/2005.html'
+                templateUrl: 'views/Leters/2005.html'
+            })
+            .when('/Albania', {
+                templateUrl: 'views/Around_world/Albania.html'
+            })
+            .when('/Austria', {
+                templateUrl: 'views/Around_world/Austria.html'
+            })
+            .when('/Czech', {
+                templateUrl: 'views/Around_world/Czech.html'
+            })
+            .when('/French', {
+                templateUrl: 'views/Around_world/French.html'
+            })
+            .when('/Germany', {
+                templateUrl: 'views/Around_world/Germany.html'
+            })
+            .when('/Holland', {
+                templateUrl: 'views/Around_world/Holland.html'
+            })
+            .when('/Italy', {
+                templateUrl: 'views/Around_world/Italy.html'
+            })
+            .when('/Netherlands', {
+                templateUrl: 'views/Around_world/Netherlands.html'
+            })
+            .when('/Poland', {
+                templateUrl: 'views/Around_world/Poland.html'
+            })
+            .when('/Ukraine', {
+                templateUrl: 'views/Around_world/Ukraine.html'
+            })
+            .when('/List_letters', {
+                templateUrl: 'views/List_letters.html'
+            })
+            .when('/Take_letter', {
+                templateUrl: 'views/Take_letter.html'
+            })
+            .when('/Take_letter_adm', {
+                templateUrl: 'views/Take_letter_adm.html'
             })
             .otherwise({redirectTo: '/'}
         );
